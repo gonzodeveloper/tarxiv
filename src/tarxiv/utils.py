13fc59980b77164d9c93e89d5f425ba19fbefa00
JulienPeloton <peloton@lal.in2p3.fr>
2024-09-19 13:37:01 +0200

    PEP8

--- src/tarxiv/utils.py
+++ src/tarxiv/utils.py
@@ -4,13 +4,11 @@ import yaml
 
 
 def read_config(config_file):
-    """
-    Read in yaml configuration file for modules.
+    """Read in yaml configuration file for modules.
+
     :param config_file: filename for config yaml; str
     :return: module configuration; dict
     """
     # Read in configuration
     with open(config_file) as stream:
         return yaml.safe_load(stream)
-
-
