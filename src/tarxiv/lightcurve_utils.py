722089fd3a57aea8a60674413cbedc04755d6ea8
JulienPeloton <peloton@lal.in2p3.fr>
2024-09-19 14:08:07 +0200

    Add utility to get Fink data from a TNS name

--- src/tarxiv/lightcurve_utils.py
+++ src/tarxiv/lightcurve_utils.py
@@ -110,7 +110,7 @@ def get_ztf_lc_from_ztf_name(ztf_name: str):
         return pd.DataFrame()
 
     if r.json() == []:
-        _LOG.warning("Data for {} not found on Fink".format(ztf_name))
+        _LOG.warning("Data for the ZTF name {} not found on Fink".format(ztf_name))
 
     # TODO: do we really need the pandas conversion?
     pdf = pd.read_json(io.BytesIO(r.content))
@@ -119,7 +119,57 @@ def get_ztf_lc_from_ztf_name(ztf_name: str):
 
 
 def get_ztf_lc_from_tns_name(tns_name: str):
-    pass
+    """Get ZTF data from Fink using a TNS object name
+
+    Parameters
+    ----------
+    tns_name: str
+        Name of a TNS object, e.g. SN 2024utu
+
+    Returns
+    -------
+    pd.DataFrame
+        Pandas DataFrame containing ZTF data from Fink for
+        the matching object in TNS. Each row is a measurement.
+
+    Examples
+    --------
+    >>> pdf = get_ztf_lc_from_tns_name("SN 2024utu")
+    >>> assert not pdf.empty, "Oooops there should be data for SN 2024utu (ZTF24abeiqfc) in Fink!"
+
+
+    # Check that crazy input returns empty output
+    >>> out = get_ztf_lc_from_tns_name("toto")
+    >>> assert out.empty, "Hum, there is no `toto` in Fink!"
+    """
+    # first use the Fink resolver to get corresponding ZTF name
+    r = requests.post(
+        "https://fink-portal.org/api/v1/resolver",
+        json={"resolver": "tns", "name": tns_name},
+    )
+
+    # check status
+    if r.status_code != 200:
+        _LOG.warning(
+            "Unable to get data for {} in Fink. HTTP error code: {}".format(
+                tns_name, r.status_code
+            )
+        )
+        return pd.DataFrame()
+
+    if r.json() != []:
+        for section in r.json():
+            # extract ZTF name
+            if section["d:internalname"].startswith("ZTF"):
+                ztf_name = section["d:internalname"]
+                return get_ztf_lc_from_ztf_name(ztf_name)
+
+    _LOG.warning(
+        "We could not find ZTF counterpart in Fink for the TNS object name {}".format(
+            tns_name
+        )
+    )
+    return pd.DataFrame()
 
 
 def get_ztf_lc_from_coord(ra: float, dec: float):
