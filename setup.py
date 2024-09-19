5c3584b98b6c5d341d49c42b287aaa2413972d2d
wintergreen <doctor.kyle.hart@gmail.com>
2024-09-19 03:19:12 -1000

    deployment

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+# Get dependencies
+with open("requirements.txt") as f:
+    install_requires = f.read().splitlines()
+
+setup(
+    name="tarxiv",
+    version="0.0.1",
+    author="Kyle Hart",
+    author_email="kylehart@hawaii.edu",
+    license="BSD-3",
+    packages=find_packages(),
+    install_requires=install_requires,
+    zip_safe=False,
+)
+
