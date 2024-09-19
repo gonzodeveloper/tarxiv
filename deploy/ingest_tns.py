47370a7d05a1c0ac0a91eb560f2dc99e102d8a8a
JulienPeloton <peloton@lal.in2p3.fr>
2024-09-19 16:36:09 +0200

    Fix missing definitions

--- deploy/ingest_tns.py
+++ deploy/ingest_tns.py
@@ -1,4 +1,5 @@
 from tarxiv.utils import read_config
+from collections import OrderedDict
 
 from couchbase.cluster import Cluster, ClusterOptions
 from couchbase.auth import PasswordAuthenticator
@@ -14,59 +15,65 @@ from pprint import pprint
 
 # Read in config
 config = read_config(sys.argv[1])
+TNS_URL = "https://www.wis-tns.org"
 
 # Create marker
 tns_marker = {
-        "tns_id": config["tns"]["id"], 
-        "type": config["tns"]["type"], 
-        "name": config["tns"]["name"]
-        }
-tns_marker_str = "tns_marker" + json.dumps(tns_marker, separators=(',', ':'))
+    "tns_id": config["tns"]["id"],
+    "type": config["tns"]["type"],
+    "name": config["tns"]["name"],
+}
+tns_marker_str = "tns_marker" + json.dumps(tns_marker, separators=(",", ":"))
 
 # Use fink to download
-json_data = [('api_key', (None, config["tns"]["api_key"])),]
+json_data = [
+    ("api_key", (None, config["tns"]["api_key"])),
+]
 
 # define header
-headers = {'User-Agent': tns_marker_str}
+headers = {"User-Agent": tns_marker_str}
 
 r = requests.post(
-      config["tns"]["site"] + '/system/files/tns_public_objects/tns_public_objects.csv.zip',
-      files=json_data,
-      headers=headers
+    config["tns"]["site"]
+    + "/system/files/tns_public_objects/tns_public_objects.csv.zip",
+    files=json_data,
+    headers=headers,
 )
 
 pprint(locals())
 # Write to bytesio and convert to pandas
 with zipfile.ZipFile(io.BytesIO(r.content)) as myzip:
-    data = myzip.read(name='tns_public_objects.csv')
+    data = myzip.read(name="tns_public_objects.csv")
 
 tns_df = pd.read_csv(io.BytesIO(data), skiprows=[0])
 
-# Sample for testing 
+# Sample for testing
 tns_df = tns_df.sample(1000)
 
 # Connect to couchbase
-connection_str = 'couchbase://' + config["database"]["host"]
-options = ClusterOptions(PasswordAuthenticator(
-    config["database"]["user"], config["database"]["pass"]))
+connection_str = "couchbase://" + config["database"]["host"]
+options = ClusterOptions(
+    PasswordAuthenticator(config["database"]["user"], config["database"]["pass"])
+)
 cluster = Cluster(options)
 # Get tarxiv access
-cb = cluster.bucket('tarxiv')
-coll = cb.collection('tns')
+cb = cluster.bucket("tarxiv")
+coll = cb.collection("tns")
 
 # Query all TNS objects in database
 for idx, tns_row in tns_df.iterrows():
     print(f"Retreiving {idx:06d} of {len(tns_df):06d}")
     # Run request to TNS server
-    get_url = TNS_URL +  "/api/get/object"
-    headers = {'User-Agent': tns_marker}
+    get_url = TNS_URL + "/api/get/object"
+    headers = {"User-Agent": tns_marker}
     obj_request = OrderedDict(
-            ("objname", tns_row['name']), 
-            ("objid", ""), 
-            ("photometry", "0"), 
-            ("spectra", "1"))
-    get_data = {'api_key': tns_api_key, 'data': json.dumps(obj_request)}
-    response = requests.post(get_url, headers = headers, data = get_data)
+        ("objname", tns_row["name"]),
+        ("objid", ""),
+        ("photometry", "0"),
+        ("spectra", "1"),
+    )
+    get_data = {"api_key": config["tns"]["api_key"], "data": json.dumps(obj_request)}
+    response = requests.post(get_url, headers=headers, data=get_data)
     payload = json.loads(response.text)["data"]["reply"]
     # Upsert to database
     doc_id = str(uuid.uuid4())
@@ -74,9 +81,3 @@ for idx, tns_row in tns_df.iterrows():
 
 print()
 cluster.close()
-
-
-
-
-
-
