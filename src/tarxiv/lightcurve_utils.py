09c5ba65d9521854007b11069725592e9b837bf9
JulienPeloton <peloton@lal.in2p3.fr>
2024-09-19 11:44:46 +0200

    Fix PEP8 complaints

--- src/tarxiv/lightcurve_utils.py
+++ src/tarxiv/lightcurve_utils.py
@@ -1,5 +1,9 @@
 """Pull and process lightcurves"""
 
+import logging
+
+_LOG = logging.getLogger(__name__)
+
 
 def mapping_ztf_to_tarxiv():
     """Mapping between ZTF column names and tarxiv column names
@@ -48,10 +52,22 @@ def get_ztf_lc(ztf_name=None, tns_name=None, coord=None):
     elif coord is not None and isinstance(coord, tuple):
         pdf = get_ztf_lc_from_coord(coord[0], coord[1])
     else:
-        log.error(
+        _LOG.error(
             "You should choose an object name or provide coordinates to get ZTF lightcurves"
         )
 
     # TODO: perform the column name conversion before returning
     # TODO: `mapping_ztf_to_tarxiv`
     return pdf
+
+
+def get_ztf_lc_from_ztf_name(ztf_name: str):
+    pass
+
+
+def get_ztf_lc_from_tns_name(tns_name: str):
+    pass
+
+
+def get_ztf_lc_from_coord(ra: float, dec: float):
+    pass
