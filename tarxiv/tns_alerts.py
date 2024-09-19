e4894360c464461a2fed27f5731ef95fcc5bae09
wintergreen <doctor.kyle.hart@gmail.com>
2024-09-19 06:46:04 -1000

    naming

--- tarxiv/tns_alerts.py
+++ tarxiv/tns_alerts.py
@@ -1,1 +1,28 @@
 # Listen for new TNS Alerts
+import requests
+import json
+
+def generate_marker(tns_info):
+    # Create marker
+    tns_marker = {
+            "tns_id": tns_info["id"],
+            "type": tns_info["type"],
+            "name": tns_info["name"]
+            }
+    return "tns_marker" + json.dumps(tns_marker, separators=(',', ':'))
+
+def get_tns_entry(objid, tns_info):
+    # Run request to TNS server
+    get_url = tns_info["site"] +  "/api/get/object"
+    headers = {'User-Agent': generate_marker(tns_info)}
+    obj_request = OrderedDict([
+            ("objid", objid),
+            ("objname", ""),
+            ("photometry", "0"),
+            ("spectra", "1")])
+    get_data = {'api_key': tns_info["api_key"], 'data': json.dumps(obj_request)}
+    response = requests.post(get_url, headers = headers, data = get_data)
+    
+    return json.loads(response.text)["data"]["reply"]
+
+
