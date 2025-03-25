b6de263157bdf1e872f6e00d1e5b1e75f311476e
JulienPeloton <peloton@lal.in2p3.fr>
2025-03-25 14:55:06 +0100

    Ruff linting

--- tarxiv/database.py
+++ tarxiv/database.py
@@ -1,5 +1,4 @@
 # Database utilities
-import datetime
 
 from .utils import read_config, get_logger
 from couchbase.cluster import Cluster
@@ -8,7 +7,6 @@ from couchbase.auth import PasswordAuthenticator
 
 
 class TarxivDB:
-
     def __init__(self, config_dir):
         # General parameters
         config = read_config(config_dir + "config.yml")
@@ -16,10 +14,14 @@ class TarxivDB:
 
         # Connect to Couchbase
         self.logger.info("connecting to couchbase")
-        connection_str = 'couchbase://' + config["database"]["host"]
-        options = ClusterOptions(PasswordAuthenticator(config["database"]["user"], config["database"]["pass"]))
+        connection_str = "couchbase://" + config["database"]["host"]
+        options = ClusterOptions(
+            PasswordAuthenticator(
+                config["database"]["user"], config["database"]["pass"]
+            )
+        )
         self.cluster = Cluster(connection_str, options)
-        self.conn = self.cluster.bucket('tarxiv')
+        self.conn = self.cluster.bucket("tarxiv")
         self.logger.info("connected")
 
     def upsert(self, doc_id, doc_payload, collection):
@@ -30,4 +32,3 @@ class TarxivDB:
 
     def close(self):
         self.cluster.close()
-
