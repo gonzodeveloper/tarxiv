8a02944f11f68a4166b3a8667661b583d0ab54f5
gonzodeveloper <doctor.kyle.hart@gmail.com>
2025-05-22 03:10:50 -1000

    updated reqs version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="tarxiv",
-    version="0.0.3",
+    version="0.0.5",
     author="Kyle Hart",
     author_email="kylehart@hawaii.edu",
     license="BSD-3",
